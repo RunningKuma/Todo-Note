46b79234fff8778dc7e48e47ed19a67e1aa38b0f
Woisol-G <Woisol-G@outlook.com>
2025-07-11 16:23:04 +0800

    :art: chore[server]: extend token expire time to 1d

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -5,7 +5,7 @@ const SECRET_KEY = process.env.JWT_SECRET || 'secret_key';
 
 //! 明文？：https://www.bilibili.com/video/BV1XCjPz4EVd
 export const generateToken = (userId: string) => {
-  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: '1h' });
+  return jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: '1d' });
 };
 
 export const verifyToken = (token: string) => {
