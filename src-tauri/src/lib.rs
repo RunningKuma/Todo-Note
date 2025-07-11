90346f5acc7b8741a5d32734f5e2ad9458779375
Woisol-G <Woisol-G@outlook.com>
2025-07-11 18:59:51 +0800

    Revert ":rocket: build: temporarily enable tauri devtool"
    This reverts commit 6e96b6528dcc31c34d382fe752569c8fa12321ba.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,21 +7,9 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default()
+    tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet]);
-
-    // 在开发模式下自动打开开发者工具
-    #[cfg(debug_assertions)]
-    {
-        builder = builder.setup(|app| {
-            let window = app.get_webview_window("main").unwrap();
-            window.open_devtools();
-            Ok(())
-        });
-    }
-
-    builder
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
